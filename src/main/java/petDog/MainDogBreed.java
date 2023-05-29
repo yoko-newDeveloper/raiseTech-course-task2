547927b5ad2343eabdea179407b98982613dc617
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-05-29 21:56:00 +0900

    task2-1/オリジナルのクラスを実装

---
+++ src/main/java/petDog/MainDogBreed.java
@@ -0,0 +1,8 @@
+package petDog;
+
+public class MainDogBreed {
+    public static void main(String[] args) {
+        PetDog myPet = new PetDog("VANILLA", "chihuahua", 5);
+        myPet.displayProfile();
+    }
+}
