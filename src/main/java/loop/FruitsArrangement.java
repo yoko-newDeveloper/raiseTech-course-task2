d1642f6b80b64959570d33fb5d27cbf11348c536
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-05-29 22:48:01 +0900

    配列を使った繰り返し処理

---
+++ src/main/java/loop/FruitsArrangement.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class FruitsArrangement {
+    public static void main(String[] args) {
+        String[] fruits = {"りんご", "バナナ", "オレンジ", "いちご"};
+        for(int i = 0; i < fruits.length; i++) {
+            System.out.println("好きなフルーツ:" + fruits[i]);
+        }
+    }
+}
+
