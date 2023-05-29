846662cb128fb605b59cb4c7d6856eead8f2e5d0
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-05-29 23:57:20 +0900

    条件分岐を実装

---
+++ src/main/java/branch/AnimalSound.java
@@ -0,0 +1,36 @@
+package branch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalSound {
+    public static void main(String[] args) {
+        List<List<String>> animalSounds = new ArrayList<>();
+        animalSounds.add(List.of("1", "わん！", "犬"));
+        animalSounds.add(List.of("2", "にゃー！", "猫"));
+        animalSounds.add(List.of("3", "ぴよ！", "鳥"));
+
+        // コマンドライン引数で数値を入力する
+        if (args.length == 0) {
+            System.out.println("数値を入力してください");
+            return;
+        }
+
+        String input = args[0];
+        boolean found = false;
+
+        for (List<String> animalSound : animalSounds) {
+            if (animalSound.get(0). equals(input)) {
+                String sound = animalSound.get(1);
+                String animal = animalSound.get(2);
+                System.out.println("鳴き声：" + sound + " 動物：" + animal);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("他の動物です");
+        }
+    }
+}
