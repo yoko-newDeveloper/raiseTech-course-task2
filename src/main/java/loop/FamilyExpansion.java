eb0926b892c8036632c28b601ca7796243d90a3d
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-05-29 22:57:16 +0900

    拡張for文

---
+++ src/main/java/loop/FamilyExpansion.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyExpansion {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+
+        //　要素を追加
+        list.add("お父さん");
+        list.add("お母さん");
+        list.add("お兄ちゃん");
+        list.add("妹");
+        list.add("猫");
+
+        // 拡張for文
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
