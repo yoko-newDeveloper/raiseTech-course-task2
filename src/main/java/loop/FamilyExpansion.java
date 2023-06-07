4a91de03e9ac2356f23a715d741045480478b5aa
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-07 20:49:36 +0900

    変数名を修正

--- src/main/java/loop/FamilyExpansion.java
+++ src/main/java/loop/FamilyExpansion.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class FamilyExpansion {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<String> familys = new ArrayList<>();
 
         //　要素を追加
-        list.add("お父さん");
-        list.add("お母さん");
-        list.add("お兄ちゃん");
-        list.add("妹");
-        list.add("猫");
+        familys.add("お父さん");
+        familys.add("お母さん");
+        familys.add("お兄ちゃん");
+        familys.add("妹");
+        familys.add("猫");
 
         // 拡張for文
-        for (String str : list) {
-            System.out.println(str);
+        for (String family : familys) {
+            System.out.println(family);
         }
     }
 }
