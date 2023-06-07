c870dc5bd99fce827aec3a0a5985761e39e8ecef
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-07 22:29:51 +0900

    Listと拡張for文の変数名を修正

--- src/main/java/loop/FamilyExpansion.java
+++ src/main/java/loop/FamilyExpansion.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class FamilyExpansion {
     public static void main(String[] args) {
-        List<String> familys = new ArrayList<>();
+        List<String> family = new ArrayList<>();
 
         //　要素を追加
-        familys.add("お父さん");
-        familys.add("お母さん");
-        familys.add("お兄ちゃん");
-        familys.add("妹");
-        familys.add("猫");
+        family.add("お父さん");
+        family.add("お母さん");
+        family.add("お兄ちゃん");
+        family.add("妹");
+        family.add("猫");
 
         // 拡張for文
-        for (String family : familys) {
-            System.out.println(family);
+        for (String member : family) {
+            System.out.println(member);
         }
     }
 }
